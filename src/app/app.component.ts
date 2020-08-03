3ccda872f3766a907cfb6841cb50ffb9c35ee0a4
Khoi Phan <khoiphan21@gmail.com>
2020-08-03 16:52:47 +1000

    add basic code to check database connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataStore } from '@aws-amplify/datastore';
+import { Task } from '../models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'todos-with-datastore';
+
+  async ngOnInit() {
+    const tasks = await DataStore.query(Task);
+    console.log('AppComponent -> ngOnInit -> tasks', tasks);
+  }
+
+  async addTask() {
+    const res = await DataStore.save(
+      new Task({
+        title: 'New Task',
+      })
+    );
+    console.log('AppComponent -> addTask -> res', res);
+  }
 }
